797659e69e6a0d8e50a5542a5336db31bec3383b
dingxiao <dingxiaohome@aol.com>
2020-03-23 20:55:39 -0400

    1 file modified

--- src/app/product-alert-ding/product-alert-ding.component.ts
+++ src/app/product-alert-ding/product-alert-ding.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alert-ding',
@@ -8,6 +9,8 @@ import { Input } from '@angular/core';
 })
 export class ProductAlertDingComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
