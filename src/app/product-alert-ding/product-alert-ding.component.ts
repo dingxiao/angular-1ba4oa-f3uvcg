027e8fd9cafb3645fceccaa01407468b61011970
dingxiao <dingxiaohome@aol.com>
2020-03-23 20:41:13 -0400

    1 file modified, 2 added

---
+++ src/app/product-alert-ding/product-alert-ding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alert-ding',
+  templateUrl: './product-alert-ding.component.html',
+  styleUrls: ['./product-alert-ding.component.css']
+})
+export class ProductAlertDingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
