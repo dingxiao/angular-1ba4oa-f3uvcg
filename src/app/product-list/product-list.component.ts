ea7ab5449103861230de339e8903f1d6e8657b5f
dingxiao <dingxiaohome@aol.com>
2020-03-23 20:58:58 -0400

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,9 +9,12 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
-
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
